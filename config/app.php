ebff6c48c884a9cc0fd42fd77e81fcbccb2ef07b
Abdullah Al Mamun <aalmamun417@gmail.com>
2018-04-01 21:04:03 +0600

    Abdullah:  import medicine file and test file

--- config/app.php
+++ config/app.php
@@ -214,16 +214,12 @@ return [
             'driver' => 'Cake\Database\Driver\Mysql',
             'persistent' => false,
             'host' => 'localhost',
-            /**
-             * CakePHP will use the default DB port based on the driver selected
-             * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
-             * the following line and set the port accordingly
-             */
-            //'port' => 'non_standard_port_number',
             'username' => 'root',
             'password' => '',
-            //'database' => 'leanhealthatstanfordform',
             'database' => 'pms',
+
+            //'port' => 'non_standard_port_number',
+            //'database' => 'leanhealthatstanfordform',
             'encoding' => 'utf8',
             //'timezone' => 'UTC',
             'timezone' => '',
